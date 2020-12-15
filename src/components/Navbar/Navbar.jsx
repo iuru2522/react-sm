964f7a76209107af3a2957db0cfd5e9be2cc19ed
Iurii Manastyrskyi <ternoboy@gmail.com>
2020-12-16 00:36:08 +0200

    added NavLinks

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,29 +1,29 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 // import classes from './Navbar.module.css';
-import './Navbar.css';
-
-
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <nav className="nav">
-      <div className='itemz'>
-        <a href="/profile">Profile</a>
+      <div className="itemz">
+      <NavLink to="/profile">Profile</NavLink>
       </div>
 
-      <div className='itemz'>
-        <a href="/dialogs">Messages</a>
+      <div className="itemz">
+        {/* <a href="/dialogs">Messages</a> */}
+        <NavLink to="/dialogs">Messages</NavLink>
       </div>
 
-      <div className='itemz'>
+      <div className="itemz">
         <a>News</a>
       </div>
 
-      <div className='itemz'>
+      <div className="itemz">
         <a>Music</a>
       </div>
 
-      <div className='itemz'>
+      <div className="itemz">
         <a>Settings</a>
       </div>
     </nav>
