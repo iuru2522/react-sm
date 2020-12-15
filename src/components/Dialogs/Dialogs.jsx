fa13809031cf0802cc8bdcbf0fdccaf0ba7fef99
Iurii Manastyrskyi <ternoboy@gmail.com>
2020-12-15 23:36:26 +0200

    add Dialogs component

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Dialogs.css';
+
+
+const Dialogs = (props) => {
+    return (
+        <div className='dialoge'>
+            Dialogs
+        </div>
+    )
+}
+
+export default Dialogs;
