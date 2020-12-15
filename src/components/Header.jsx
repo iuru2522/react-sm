be241a71e88b3cde213a91cabcd82f81594cd7f5
Iurii Manastyrskyi <ternoboy@gmail.com>
2020-12-15 19:55:29 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://www.logolynx.com/images/logolynx/03/03459664b36ef0ec4bcebaa4d6eec299.jpeg" />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div clasName="content">
+      <div className='insidec'>
+        <img src="https://ak.picdn.net/shutterstock/videos/32692618/thumb/1.jpg" />
+      </div>
+
+      <div>ava + description</div>
+
+      <div>
+        My posts
+        <div>New Post</div>
+        <div>
+          <div>post 1</div>
+
+          <div>post 2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
