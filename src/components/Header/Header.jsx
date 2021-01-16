42364f37e7a0c56ad06e9c95ad736e19dd1f54e7
Iurii Manastyrskyi <ternoboy@gmail.com>
2021-01-16 23:57:46 +0200

    cookie, login theory, auth/me

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import './Header.css'
+import { NavLink } from "react-router-dom";
+import "./Header.css";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header className="header">
       <img src="https://www.logolynx.com/images/logolynx/03/03459664b36ef0ec4bcebaa4d6eec299.jpeg" />
+      <div className="loginBlock">
+        {props.isAuth ? props.login : <NavLink to={"/login"}>Login</NavLink>}
+      </div>
     </header>
   );
 };
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -67,29 +67,6 @@ let mapStateToProps = (state) => {
   };
 };
 
-// let mapDispatchToProps = (dispatch) => {
-//   return {
-//     follow: (userId) => {
-//       dispatch(followActionCreator(userId));
-//     },
-//     unfollow: (userId) => {
-//       dispatch(unfollowActionCreator(userId));
-//     },
-//     setUsers: (users) => {
-//       dispatch(setUsersActionCreator(users));
-//     },
-//     setCurrentPage: (pageNumber) => {
-//       dispatch(setCurrentPageActionCreator(pageNumber));
-//     },
-//     setTotalUsersCount: (totalCount) => {
-//       dispatch(setTotalUsersCountActionCreator(totalCount));
-//     },
-//     toggleIsFetching: (isFetching) => {
-//       dispatch(toggleIsFetchingActionCreator(isFetching));
-//     },
-//   };
-// };
-
 export default connect(mapStateToProps,  {
   follow,
   unfollow,
