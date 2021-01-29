6370f72ca705c1100bcb715ceb33d443d2324bd0
Iurii Manastyrskyi <ternoboy@gmail.com>
2021-01-29 23:54:04 +0200

    redux-form

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -28,24 +28,21 @@ class ProfileStatus extends React.Component {
     }
 
     onStatusChange = (e) => {
+        this.setState({
+            status: e.currentTarget.value
+        });
+    }
+
+    componentDidUpdate (prevProps, prevState) {
         if (prevProps.status !== this.props.status) {
-
-
-            this.setState({
-                status: e.currentTarget.value
-            });
-        }
-
-
+        this.setState({status: this.props.status});
+    }
     }
 
-    componentDidUpdate(prevProps, prevState) {
-        this.setState({ status: this.props.status });
-
-    }
+    
 
     render() {
-
+        
         return (
             <div>
                 {!this.state.editMode &&
