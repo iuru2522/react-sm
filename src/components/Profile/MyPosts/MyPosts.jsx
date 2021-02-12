ca621f0142f7f1e9360405f00cc7f012869a4cfb
Iurii Manastyrskyi <ternoboy@gmail.com>
2021-02-12 23:57:18 +0200

    shouldComponentUpdate, memo, PureComponent

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,57 +1,37 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
-// import {
-//   addPostActionCreator,
-//   updateNewPostTextActionCreator,
-// } from "../../../redux/profile-reducer";
 import "./MyPosts.css";
 import Post from "./Post/Post";
 import {maxLengthCreator, required} from "../../../utils/validators/validators";
 import { Textarea } from "../../common/FormsControls/FormsControls";
 
-// let addPostActionCreator = () => {
-//   return {
-//     type: 'ADD-POST'
-//   }
-// }
-
-// let updateNewPostTextActionCreator = (text) => {
-//   return { type: 'UPDATE-NEW-POST-TEXT', newText: text};
-// }
-
 const maxLength10 = maxLengthCreator(10);
 
-const MyPosts = (props) => {
-  // let posts = [
-  //   { id: 1, post: "Hello, there", likesCount: 12 },
-  //   { id: 2, post: "Its my first post", likesCount: 10 },
-  //   { id: 3, post: "ts my second post", likesCount: 5 },
-  // ];
+// const MyPosts = (props) => {
 
-  let postsElements = props.posts.map((p) => (
-    <Post message={p.post} likesCount={p.likesCount} />
-  ));
+
+//   let postsElements = props.posts.map((p) => (
+//     <Post message={p.post} likesCount={p.likesCount} />
+//   ));
+
+  const MyPosts = React.memo(props => {
+    console.log("RENDER YO");
+    let postsElements =
+        props.posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>);
 
   let newPostElement = React.createRef();
 
   let onAddPost = (values) => {
     props.addPost(values.newPostText  );
 
-    // props.dispatch(addPostActionCreator()); //addPost
-    // props.updateNewPostText('');
   };
-
-  // let onPostChange = () => {
-  //   let text = newPostElement.current.value;
-  //   props.updateNewPostText(text)
-  //   let action = updateNewPostTextActionCreator(text);
-  //   props.dispatch(action); //updateNewPostText
-  // };
 
   return (
     <div className="postsblock">
       <h2>My posts</h2>
-      <AddNewPostForm onSubmit={onAddPost} />
+
+      <AddNewPostFormRedux onSubmit={onAddPost}/>
+      {/* <AddNewPostForm onSubmit={onAddPost} /> */}
       <div className="items">{postsElements}</div>
     </div>
   );
@@ -70,6 +50,6 @@ function AddNewPostForm (props) {
       </div>
     </form>
   )
-}
+  }
 
 export default MyPosts;
